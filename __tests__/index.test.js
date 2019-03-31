62959499ce6fd81eddd55431a1cee9c47bc7d70d
Mark Metzger <markmetzger@utexas.edu>
2019-03-31 15:18:07 -0500

    initial commit

---
+++ __tests__/index.test.js
@@ -0,0 +1,7 @@
+import fix from '../src/index';
+
+
+
+test('Test enhancer is function', () => {
+  expect(typeof SVGFEColorMatrixElement === 'function').toBe(true);
+});
---
+++ src/index.js
@@ -0,0 +1,36 @@
+
+
+const bind = (actionCreators, dispatch) => {
+    const obj = {};
+    for (const key of Object.keys(actionCreators)) {
+        obj[key] = (...params) => dispatch(actionCreators[key](...params));
+    }
+    return obj;
+}
+
+const enhance = (connect) => {
+    let _state_;
+    const getState = () => _state_;
+    return (mapStore, mapState, mapDispatch, ...params ) => {
+        const newMapState = (state, ownProps) => {
+            _state_ = state;
+            return mapState ? mapState(state, ownProps) : {};
+        }
+        const newMapDispatch = (dispatch, ownProps) => {
+            let result;
+            if (!mapDispatch) {
+                result = {};
+            } else if (typeof mapDispatch === 'object') {
+                result = bind(mapDispatch, dispatch);
+            } else {
+                result = mapDispatch(dispatch, ownProps);
+            }
+            mapStore && mapStore(dispatch, getState, ownProps);
+            return result;
+        }
+        return connect(mapState, mapDispatch, ...params);
+    }
+}
+
+export const withStore = enhance;
+export default enhance;
